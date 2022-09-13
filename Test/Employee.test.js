a57e0504580042372e5db1a9b9002f96a601af89
Mohamed Mohamed <mohamedmohamed@Mohameds-MacBook-Pro-2.local>
2022-09-12 23:27:42 -0500

    2nd Commit

---
+++ Test/Employee.test.js
@@ -0,0 +1,16 @@
+class Employee {
+    constructor(name,id,email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.title = "Employee"
+
+    }
+    
+    getName (){
+        return this.name;
+    }
+    getId(){
+        return this.id;
+    }
+}
